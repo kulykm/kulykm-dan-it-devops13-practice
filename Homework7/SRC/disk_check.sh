9259f331560eca2fe47e25a1cf18df4e06738274
kulykm <12dev.max@gmail.com>
2026-01-28 03:51:11 +0000

    HW7: Завдання 2 — додано cron-завдання для disk_check.sh + ручна перевірка роботи скрипта

--- Homework7/SRC/disk_check.sh
+++ Homework7/SRC/disk_check.sh
@@ -6,6 +6,8 @@ THRESHOLD=$1
 # Отримати використання кореневого розділу /
 USAGE=$(df / | tail -1 | awk '{print $5}' | sed 's/%//')
 
+echo "USAGE=$USAGE, THRESHOLD=$THRESHOLD"
+
 # Якщо перевищено поріг — записати попередження у лог
 if [ "$USAGE" -gt "$THRESHOLD" ]; then
     echo "$(date): WARNING - Disk usage is ${USAGE}% (threshold ${THRESHOLD}%)" >> /var/log/disk.log
