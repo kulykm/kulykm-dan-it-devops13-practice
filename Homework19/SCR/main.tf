c1712828316e2f94e73086c533c57ac78ac4f2d2
kulykm <12dev.max@gmail.com>
2026-06-04 13:14:49 +0000

    Update main.tf to final version

--- Homework19/SCR/main.tf
+++ Homework19/SCR/main.tf
@@ -6,13 +6,14 @@ terraform {
     }
   }
 
-  required_version = ">= 1.3.0"
+  required_version = ">= 1.0.0"
 }
 
 provider "aws" {
   region = "eu-central-1"
 }
 
+# VPC
 resource "aws_vpc" "main" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
@@ -23,6 +24,7 @@ resource "aws_vpc" "main" {
   }
 }
 
+# Internet Gateway
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
 
@@ -31,6 +33,7 @@ resource "aws_internet_gateway" "igw" {
   }
 }
 
+# Public Subnet
 resource "aws_subnet" "public_subnet" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
@@ -42,6 +45,7 @@ resource "aws_subnet" "public_subnet" {
   }
 }
 
+# Route Table for public subnet
 resource "aws_route_table" "public_rt" {
   vpc_id = aws_vpc.main.id
 
@@ -50,14 +54,153 @@ resource "aws_route_table" "public_rt" {
   }
 }
 
+# Route to Internet for public subnet
 resource "aws_route" "public_internet_access" {
   route_table_id         = aws_route_table.public_rt.id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.igw.id
 }
 
+# Associate Route Table with Public Subnet
 resource "aws_route_table_association" "public_assoc" {
   subnet_id      = aws_subnet.public_subnet.id
   route_table_id = aws_route_table.public_rt.id
 }
 
+# Security Group for public EC2 (bastion)
+resource "aws_security_group" "public_sg" {
+  name        = "hw19-public-sg"
+  description = "Allow SSH from internet"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "SSH from anywhere"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "hw19-public-sg"
+  }
+}
+
+# Public EC2 Instance (bastion)
+resource "aws_instance" "public_ec2" {
+  ami                         = "ami-0e872aee57663ae2d" # Ubuntu 22.04 LTS (eu-central-1)
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.public_subnet.id
+  vpc_security_group_ids      = [aws_security_group.public_sg.id]
+  associate_public_ip_address = true
+  key_name                    = "hw19-key"
+
+  tags = {
+    Name = "hw19-public-ec2"
+  }
+}
+
+############################
+# Private part starts here #
+############################
+
+# Private Subnet
+resource "aws_subnet" "private_subnet" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "eu-central-1a"
+
+  tags = {
+    Name = "hw19-private-subnet"
+  }
+}
+
+# Elastic IP for NAT Gateway
+resource "aws_eip" "nat_eip" {
+  domain = "vpc"
+
+  tags = {
+    Name = "hw19-nat-eip"
+  }
+}
+
+# NAT Gateway in public subnet
+resource "aws_nat_gateway" "nat_gw" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.public_subnet.id
+
+  tags = {
+    Name = "hw19-nat-gw"
+  }
+
+  depends_on = [aws_internet_gateway.igw]
+}
+
+# Route Table for private subnet
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "hw19-private-rt"
+  }
+}
+
+# Route from private subnet to Internet via NAT
+resource "aws_route" "private_internet_access" {
+  route_table_id         = aws_route_table.private_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.nat_gw.id
+}
+
+# Associate Route Table with Private Subnet
+resource "aws_route_table_association" "private_assoc" {
+  subnet_id      = aws_subnet.private_subnet.id
+  route_table_id = aws_route_table.private_rt.id
+}
+
+# Security Group for private EC2 (allow SSH only from public EC2)
+resource "aws_security_group" "private_sg" {
+  name        = "hw19-private-sg"
+  description = "Allow SSH only from public EC2"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description     = "SSH from public EC2"
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    security_groups = [aws_security_group.public_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "hw19-private-sg"
+  }
+}
+
+# Private EC2 Instance (no public IP)
+resource "aws_instance" "private_ec2" {
+  ami                         = "ami-0e872aee57663ae2d" # Ubuntu 22.04 LTS (eu-central-1)
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.private_subnet.id
+  vpc_security_group_ids      = [aws_security_group.private_sg.id]
+  associate_public_ip_address = false
+  key_name                    = "hw19-key"
+
+  tags = {
+    Name = "hw19-private-ec2"
+  }
+}
