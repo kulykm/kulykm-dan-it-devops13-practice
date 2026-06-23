a77a8ad108c5abdaefe231ff2c77976390f58d71
kulykm <12dev.max@gmail.com>
2026-06-23 11:01:53 +0000

    Update backend.tf with unique S3 bucket name

--- Homework20/SRC/backend.tf
+++ Homework20/SRC/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-state-danit-devops"
+    bucket = "terraform-state-mkulyk-2026"
     key    = "maksym/homework20/terraform.tfstate"
     region = "eu-central-1"
   }
