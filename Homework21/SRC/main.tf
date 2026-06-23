9836ddbc3f7ef288d171964876ac616bb89d2837
kulykm <12dev.max@gmail.com>
2026-06-23 11:41:52 +0000

    Add initial Terraform structure for HW21

---
+++ Homework21/SRC/main.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  region = "eu-central-1"
+}
+
+resource "aws_instance" "web" {
+  count         = 2
+  ami           = "ami-0c55b159cbfafe1f0" # замінимо пізніше
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "HW21-${count.index}"
+  }
+}
+
+output "public_ips" {
+  value = aws_instance.web[*].public_ip
+}
